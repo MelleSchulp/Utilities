88e9a7f5044e59ef920508901ae5055d09ebf01c
Melle Schulp <m.schulp@nin.knaw.nl>
2021-11-30 10:39:51 +0100

    Added methods to add and set table cells to the TableRowInterface.

--- Table/Interfaces/TableRowInterface.php
+++ Table/Interfaces/TableRowInterface.php
@@ -5,12 +5,20 @@ declare(use_strict=1);
 namespace Table\Interfaces;
 
 use Table\Interfaces\HasAttributesInterface;
+use Table\Interfaces\TableCellInterface;
 
 interface TableRowInterface extends HasAttributesInterface
 {
+    public function addCell(TableCellInterface $cell): void;
+
     /**
      * @return \Traversable<TableCellInterface>
      */
-    public function getCells(): \Traversable;
+    public function getCells(): ?\Traversable;
     public function hasCells(): bool;
+
+    /**
+     * @param \Traversable<TableCellInterface> $cells
+     */
+    public function setCells(\Traversable $cells): void;
 }
