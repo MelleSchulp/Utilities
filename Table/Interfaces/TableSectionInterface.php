d61feded04a17a6043fc9153f5f84f0e6ae69fed
Melle Schulp <m.schulp@nin.knaw.nl>
2021-11-30 09:58:06 +0100

    Added methods to the TableSectionInterface for adding and setting rows and section type.

--- Table/Interfaces/TableSectionInterface.php
+++ Table/Interfaces/TableSectionInterface.php
@@ -10,10 +10,17 @@ use Interfaces\TableRowInterface;
 
 interface TableSectionInterface extends HasAttributesInterface
 {
+    public function addRow(TableRowInterface $row): void;
+
     /**
      * @return \Traversable<TableRowInterface>
      */
     public function getRows(): \Traversable;
     public function getType(): TableSectionType;
     public function hasRows(): bool;
+    /**
+     * @param \Traversable<TableRowInterface> $rows
+     */
+    public function setRows(\Traversable $rows): void;
+    public function setType(TableSectionType $type): void;
 }
