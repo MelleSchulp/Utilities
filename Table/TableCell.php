839e3ba931496a643670c925d208d38445d2c611
Melle Schulp <m.schulp@nin.knaw.nl>
2021-11-30 11:03:58 +0100

    Added implementation of the TableCellInterface.

---
+++ Table/TableCell.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(use_strict=1);
+
+namespace Table\Interfaces;
+
+use Table\Enums\TableCellType;
+use Table\Traits\HasAttributesTrait;
+
+class TableCell implements TableCellInterface
+{
+    use HasAttributesTrait;
+
+    private ?string $content;
+
+    private TableCellType $type;
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function getType(): TableCellType
+    {
+        return $this->type;
+    }
+
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    public function setType(TableCellType $type): void
+    {
+        $this->type = $type;
+    }
+}
